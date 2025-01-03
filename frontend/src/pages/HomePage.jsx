399574c1ae39db4fc54938dce1e080a04be467af
golneshin <59548304+golneshin@users.noreply.github.com>
2025-01-03 12:36:44 +0330

    Card compleation

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
-import Card from "../components/Card"
-import { products } from "../products"
+import Card from "../components/Card";
+import { products } from "../products";
 
 const HomePage = () => {
   return (
-    <div>
-        {products.map(product => {
-            <Card product={product} key={product._id}/>
-        })}
+    <div className="flex flex-wrap justify-center gap-4 py-4">
+      {products.map((product) => {
+        return <Card product={product} key={product._id} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
